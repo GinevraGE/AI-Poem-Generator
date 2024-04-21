f6c1e7649d886580b96f03eee31c90354e68be89
GinevraGE <117026341+GinevraGE@users.noreply.github.com>
2024-04-21 21:12:39 +0200

    updated blink code on JS for improved Ux

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `⏳ Generating a romantic poem about<div class="generating">... ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `⏳ Generating a romantic poem about<scan class="generating">... ${instructionsInput.value}</scan>`;
 
   axios.get(apiUrl).then(displayNewPoem);
 }
