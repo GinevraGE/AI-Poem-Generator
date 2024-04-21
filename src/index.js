df8e4f5ef629e77b2aa000ac70b76a07c6d6c6c8
GinevraGE <117026341+GinevraGE@users.noreply.github.com>
2024-04-21 12:07:02 +0200

    added Axios and edited JS

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayNewPoem(response) {
   new Typewriter("#poem", {
-    strings: "Diamonds are sparkly, but none as brilliant as you are!",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
+
+function generatePoem(event) {
+  event.preventDefault();
+}
+
+let instructionsInput = document.querySelector("#user-instructions");
+let apiKey = "dfa836f02b3ot61e0f995ec04f06a183";
+let prompt = `User instructions:Generate unique and beautiful poems about ${instructionsInput.value}`;
+let context =
+  "You are an excellent romantic poem generator, your mission is to generate  a 4 line romantic poem in basic HTML,do not include a title to the poem and make sure to follow the User instructions ";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+axios.get(apiUrl).then(displayNewPoem);
 
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
