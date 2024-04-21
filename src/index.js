6cc3421e48de47759f4c5073e891a11da7801ec9
GinevraGE <117026341+GinevraGE@users.noreply.github.com>
2024-04-21 12:12:56 +0200

    corrected function curly bracket error

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ function displayNewPoem(response) {
 
 function generatePoem(event) {
   event.preventDefault();
-}
 
-let instructionsInput = document.querySelector("#user-instructions");
-let apiKey = "dfa836f02b3ot61e0f995ec04f06a183";
-let prompt = `User instructions:Generate unique and beautiful poems about ${instructionsInput.value}`;
-let context =
-  "You are an excellent romantic poem generator, your mission is to generate  a 4 line romantic poem in basic HTML,do not include a title to the poem and make sure to follow the User instructions ";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-axios.get(apiUrl).then(displayNewPoem);
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "dfa836f02b3ot61e0f995ec04f06a183";
+  let prompt = `User instructions:Generate unique and beautiful poems about ${instructionsInput.value}`;
+  let context =
+    "You are an excellent romantic poem generator, your mission is to generate  a 4 line romantic poem in basic HTML,do not include a title to the poem and make sure to follow the User instructions ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayNewPoem);
+}
 
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
